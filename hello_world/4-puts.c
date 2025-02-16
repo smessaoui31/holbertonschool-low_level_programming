92469607c9896a98bad79a38b6f2307613c92863
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-16 11:04:29 +0000

    Printing a c programm witht the puts command

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+	/** Main = Entry point of our command
+	 *
+	 * Use command puts to print 
+	 *
+	 * Return :Always return to c (success)
+	 * */
 	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
