e4716e3ad95f33c2aeddef7d134dfa264a24cc53
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-16 11:15:49 +0000

    Printing a c programm witht the puts command

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 	/**
-	 * main - Entry point of our command
+	 * main - Entry point
 	 *
 	 * Use command puts to print
 	 *
 	 * Return :Always return to 0 (success)
 	 */
 	int main(void)
+	/**
+	 * Write a C program that prints exactly a specific line
+	 *
+	 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
