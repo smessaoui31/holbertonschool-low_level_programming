822e6dc93af0d27789fbba83dd7a0cbb176e8605
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-17 15:56:09 +0000

    If and else if conditions

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * include means the different headers in my prog code
+ * The main - my entire main in the programm
+ * if means my different conditions to generate my rng
+ * else meane the other conditions to my rng
+ * return (0) always to success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf(" %d is positive \n", n);
+}
+else if (n == 0)
+{
+printf(" %d is 0\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+	}
+	return (0);
+}
