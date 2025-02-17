97f1a34b7b2d655c1b1d71d16484dada134ee62c
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-17 09:37:25 +0000

    Print the size of various type

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - the function main of my programm
+* sizeof means the size of various type
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	sizeof(int);
+	return (O);
+}
