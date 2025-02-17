03f9827918d7ef3e1105b2ce82c85393ef2d3546
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-17 08:22:35 +0000

    Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-	/**
-	* main - Prints "Programming is like building a multilingual puzzle
-	*
-	* Return: Always 0 (Success)
-	*/
-	int main(void)
+/**
+* main - Prints "Programming is like building a multilingual puzzle
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
