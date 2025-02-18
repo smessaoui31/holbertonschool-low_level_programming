c3b73bf3effa2729dc17c252eaaa3cf9c0200102
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-18 15:37:35 +0000

    Print the Base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 97; ch <= 102; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
