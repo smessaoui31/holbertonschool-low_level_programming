3043852e04aceb13e57cc1218c7adff3a84d8bc8
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-18 09:21:35 +0000

    Display the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	putchar('\n');
+	}
+	return (0);
+}
