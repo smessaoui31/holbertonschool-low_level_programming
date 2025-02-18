247df37b78964b93dc7c0ff828fb774118058e3a
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-18 16:54:26 +0000

    Print a combo of different character

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 48 ; ch <= 57 ; ch++)
+	{
+		if (ch == 9)
+			break;
+	putchar(ch);
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+return (0);
+}
