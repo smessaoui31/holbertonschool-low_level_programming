03694902477744c83d3ffb870f2336d32390094b
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-18 14:20:26 +0000

    Print serial number

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints if numbers
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+	return (0);
+}
