e980bf711a98d593416f8b8a80184a583766534e
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-18 15:18:33 +0000

    Display the reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 122; ch >= 97; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
