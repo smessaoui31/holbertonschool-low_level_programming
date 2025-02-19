aab5e368ece55cac5af912bd63c0e27e0bd168b9
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-19 14:06:58 +0000

    Print Puntchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Printsf a specific character
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+
+	putchar('\n');
+
+	return (0);
+}
