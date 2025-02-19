b324c8794e75c925c11f1d8a659ee4cdb966d8d7
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-19 15:23:45 +0000

    Print the alphabet with _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
