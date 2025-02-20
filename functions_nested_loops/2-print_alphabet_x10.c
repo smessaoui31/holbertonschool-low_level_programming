217a1a44621f8aa34bc7134c11742454b19338c6
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-20 15:40:49 +0000

    Print 10 times the alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+
+
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+	while (i < 10)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+
+		}
+		_putchar('\n');
+		i++;
+	}
+}
