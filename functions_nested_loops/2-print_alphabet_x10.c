44568d3065c9d04f81a79288818a7183f7b7d578
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-20 15:43:01 +0000

    Print 10 times the alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
-
-
-
-
-
+/**
+  * print_alphabet_x10 - Make alphabet x10 times
+  *
+  * Return: void
+  */
 void print_alphabet_x10(void)
 {
 	int i = 0;
 	char c;
+
 	while (i < 10)
 	{
 		for (c = 'a' ; c <= 'z' ; c++)
