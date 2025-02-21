4c6f31ab0e9dce036b07915b87aec76dcc310226
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-21 08:51:22 +0000

    prints the sign of a number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,18 +13,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('1');
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar ('-');
+		_putchar (45);
 		return (-1);
 	}
 
 	else
 	{
-		_putchar ('0');
+		_putchar (48);
 		return (0);
 	}
 }
