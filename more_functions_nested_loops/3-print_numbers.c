a4685c46d7c8ae9add93dbdc65154239e4ae7b14
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-02-24 10:56:09 +0000

    Print the number between 0 and 9

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
+/**
+ * print_numbers - Prints the numbers
+ *
+ *@c - the character to display
+ */
 
 
 void print_numbers(void)
 {
 	char c;
 
-	for(c = 48 ; c <= 57 ; c++)
+	for (c = 48 ; c <= 57 ; c++)
 	{
 		_putchar(c);
 	}
