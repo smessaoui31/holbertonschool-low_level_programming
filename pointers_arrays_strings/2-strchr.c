53ce723cdd643c0f798a4b35f4a53b0ee212e7c8
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-03-04 09:33:19 +0000

    My second commit for the pointers project

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,27 @@
 #include "main.h"
+#include <stdlib.h>
 
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: the string to check
+ * @c: the character we're looking for
+ *
+ * Return: a pointer to the first occurance of the character @c in the string
+ */
 
 
 char *_strchr(char *s, char c)
 {
 
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		s++;
+	}
+	return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,3 +19,4 @@ char *leet(char *);
 void reverse_array(int *a, int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
