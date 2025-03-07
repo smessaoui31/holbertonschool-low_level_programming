8008ac26864e577970878f56e6bc351b075699a1
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-03-07 17:05:59 +0000

    Second commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{ 
+	int i;
+
+	printf("Number of arguments : %d\n", argc);
+
+	for (i = 0; i < argc; i ++)
+	{
+		printf("Arguments %d : %s\n", i, argv[i]);
+
+	}
+	return (0);
+}
