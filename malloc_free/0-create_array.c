d129f02e390d5c0f94cfdb70be448ed638e25107
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-03-11 12:52:27 +0000

    First commit

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char *create_array(unsigned int size, char c)
+{
+	size = malloc(sizeof(c));
+	if (size = 0)
+	{
+		return (NULL);
+	}
+	return (0);
+}
+
