2346e35269fbcd949d22f207f6436142b2e1c77f
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-03-12 08:40:49 +0000

    Final fix and final commit , some problem with the third functions

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,15 +1,33 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * create_array - function that creates an array of chars, and initializes it
+ *                with a specific char.
+ *
+ * @size: This is the length of the array
+ * @c: This is the input character
+ *
+ * Return: An Array Initialized with the specific char
+ */
 
 char *create_array(unsigned int size, char c)
 {
-	size = malloc(sizeof(c));
-	if (size = 0)
-	{
-		return (NULL);
-	}
-	return (0);
-}
+	char *a;
+	unsigned int i;
 
+
+	if (size == 0)
+
+		return (NULL);
+
+	a = malloc(size * sizeof(char));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < size ; i++)
+		a[i] = c;
+	return (a);
+
+}
