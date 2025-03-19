174791696b9d8c27dbc7bc2ded601626392eb0ca
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-03-19 08:44:59 +0000

    Resolving the problem of an undeclared variable

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,18 +17,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
-	if (array == NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
-	if (size <= 0)
-		return (-1);
-	if (cmp == NULL)
-		return (-1);
-	for (index = 0; a < size; index++)
+
+	for (index = 0; index < size; index++)
 	{
-		if ((cmp(array[index])) == 1)
-		{
+		if (cmp(array[index]) != 0)
 			return (index);
-		}
 	}
+
 	return (-1);
 }
