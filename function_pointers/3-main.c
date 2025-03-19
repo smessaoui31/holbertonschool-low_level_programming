ca27cdb3d414a521c8c045dd2165b2c36275967d
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-03-19 09:30:22 +0000

    Create the mainc and fixing some error on my compilation

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,43 +1,37 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
+ * main - This function we have a two number and an operator
+ * @ac: Is the length of Number of arguments
+ * @av: These are the arguments
+ * Return: Return to 0
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int ac, char *av[])
 {
-	int num1, num2;
-	char *op;
+	int numb1, numb2;
+	int (*fun)(int, int);
+	char *c = av[2];
 
-	if (argc != 4)
+	if (ac != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if ((*c != '+' && *c != '-' && *c != '*' && *c != '/' && *c != '%') ||
+			*(c + 1) != 0)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	if ((*op == '/' && num2 == 0) ||
-			(*op == '%' && num2 == 0))
+	numb1 = atoi(av[1]);
+	numb2 = atoi(av[3]);
+	if ((*c == '/' || *c == '%') && numb2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
-
+	fun = get_op_func(c);
+	printf("%d\n", fun(numb1, numb2));
 	return (0);
 }
