fe2f8efe3c48a609ad77a4421403b268edf83f8c
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-04-04 07:41:18 +0000

    Fixing the kernel style because too much line on my code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,58 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given index
- * @h: Double pointer to the head of the list
- * @idx: Index where the new node should be added (starts at 0)
- * @n: Value to store in the new node
+ * insert_dnodeint_at_index - insert a new node at a given index
+ * @h: double pointer to the head of the list
+ * @idx: index where the new node should be added
+ * @n: value to assign to the new node
  *
- * Return: Address of the new node, or NULL if it failed
+ * Return: address of the new node, or NULL on failure
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp = *h;
 	unsigned int i = 0;
 
-	if (h == NULL)
-		return (NULL);
+	if (!h)
+		return NULL;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
+	if (!new_node)
+		return NULL;
 
 	new_node->n = n;
 
-	if (idx == 0)
-	{
+	if (idx == 0) {
 		new_node->prev = NULL;
 		new_node->next = *h;
-
-		if (*h != NULL)
+		if (*h)
 			(*h)->prev = new_node;
-
 		*h = new_node;
-		return (new_node);
+		return new_node;
 	}
 
-	while (temp != NULL && i < idx - 1)
-	{
+	while (temp && i < idx - 1) {
 		temp = temp->next;
 		i++;
 	}
 
-	if (temp == NULL)
-	{
+	if (!temp) {
 		free(new_node);
-		return (NULL);
+		return NULL;
 	}
 
 	new_node->next = temp->next;
 	new_node->prev = temp;
-
-	if (temp->next != NULL)
+	if (temp->next)
 		temp->next->prev = new_node;
-
 	temp->next = new_node;
 
-	return (new_node);
+	return new_node;
 }
